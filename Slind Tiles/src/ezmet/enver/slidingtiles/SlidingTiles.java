bfa0852e99be8eca5a524c837e2202930b8bbd49
EnverJ <Enlem1987@gmail.com>
2022-08-11 22:46:10 -0600

    project_6

---
+++ Slind Tiles/src/ezmet/enver/slidingtiles/SlidingTiles.java
@@ -0,0 +1,12 @@
+package ezmet.enver.slidingtiles;
+
+import javax.swing.JFrame;
+
+public class SlidingTiles extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
