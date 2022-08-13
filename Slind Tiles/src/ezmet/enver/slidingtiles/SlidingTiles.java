94c8c5b4565f549342af2322287ae89bf4538d6f
EnverJ <Enlem1987@gmail.com>
2022-08-12 23:05:04 -0600

    resize

--- Slind Tiles/src/ezmet/enver/slidingtiles/SlidingTiles.java
+++ Slind Tiles/src/ezmet/enver/slidingtiles/SlidingTiles.java
@@ -1,12 +1,43 @@
 package ezmet.enver.slidingtiles;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import ezmet.enver.mycomponents.TitleLabel;
 
 public class SlidingTiles extends JFrame {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final long serialVersionUID = 1L;
 
+	public SlidingTiles() {
+		initGUI();
+		setTitle("Sliding Tiles");
+		setResizable(true);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private void initGUI() {
+		TitleLabel titleLabel = new TitleLabel("Sliding Tiles");
+		add(titleLabel, BorderLayout.PAGE_START);
+
+	}
+
+	public static void main(String[] args) {
+		String className = UIManager.getCrossPlatformLookAndFeelClassName();
+		try {
+			UIManager.setLookAndFeel(className);
+		} catch (Exception e) {
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new SlidingTiles();
+			}
+		});
 	}
 
 }
